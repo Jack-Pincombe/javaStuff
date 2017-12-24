ac2266b09778a388fcea5145b5b2db34505722a2
Jack Pincombe <jackpincombe1@gmail.com>
2017-12-24 12:03:53 +0000

    Comments added

--- login/src/login/Login.java
+++ login/src/login/Login.java
@@ -30,6 +30,7 @@ public class Login extends Application {
     @Override
     public void start(Stage primaryStage) {
         
+        //creating the grid
         GridPane grid = new GridPane();
         grid.setAlignment(Pos.CENTER);
         grid.setHgap(10);
@@ -38,6 +39,7 @@ public class Login extends Application {
         
         Scene scene = new Scene(grid,300,200);
         
+        //adding the various text blocks and input blocks
         Text scenetitle = new Text("Please login");
         scenetitle.setFont(Font.font("Tahoma",FontWeight.NORMAL,20));
         Label userName = new Label("User Name:");
@@ -45,29 +47,34 @@ public class Login extends Application {
         Label pw = new Label("Password:");
         PasswordField pwBox = new PasswordField();
         
+        //adding above blocks to grid
         grid.add(scenetitle,0,0,2,1);
         grid.add(userName,0,1);
         grid.add(userTextField,1,1);
         grid.add(pw,0,2);
         grid.add(pwBox,1,2);
         
+        //while true grid lines will show
         grid.setGridLinesVisible(true);
         
+        //Button that will submit the info
         Button btn = new Button("Sign in");
         HBox hbBtn = new HBox(10);
         hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
         hbBtn.getChildren().add(btn);
         grid.add(hbBtn,1,4);
         
+        //space for confirmation message
         final Text actiontarget = new Text();
         grid.add(actiontarget,1,6);
         
+        //output to confirm submit button has been clicked
         btn.setOnAction(event -> {
            actiontarget.setFill(Color.FIREBRICK);
            actiontarget.setText("Sign in button pressed");
         });
         
-        
+        //importing the css 
         scene.getStylesheets().add(Login.class.getResource("Login.css").toExternalForm());
         primaryStage.setTitle("User login form");
         primaryStage.setScene(scene);
