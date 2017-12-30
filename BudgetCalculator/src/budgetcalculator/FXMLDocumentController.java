1c92e25fcc963645f9630360cd5919573fad1479
Jack-Pincombe <jackpincombe1@gmail.com>
2017-12-30 18:46:21 +0000

    now converting the inputs into ints

--- BudgetCalculator/src/budgetcalculator/FXMLDocumentController.java
+++ BudgetCalculator/src/budgetcalculator/FXMLDocumentController.java
@@ -10,31 +10,88 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import javafx.stage.Window;
 
 /**
  *
  * @author jackpincombe
  */
-public class FXMLDocumentController implements Initializable {
+public class FXMLDocumentController  {
     
     @FXML
     private Label label;
-    
     @FXML
     private Text actiontarget;
+    @FXML
+    private TextField Income;
+    @FXML
+    private TextField Rent;
+    @FXML
+    private TextField Util;
+    @FXML
+    private TextField Groceries;
+    @FXML
+    private TextField Travel;
+    @FXML
+    private TextField Debts;
+    @FXML
+    private int total;
     
+    private boolean empty = true; 
     
     @FXML
     private void handlecalculationButtonAction(ActionEvent event) {
         System.out.println("Calculation Button has been pressed");
-        actiontarget.setText("Calculation Button has been pressed");
+        System.out.println(Income.getText());
+        validate();
+        calculate();
+        System.out.println(total);
+        //actiontarget.setText(Income);
     }
     
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+    //func to calculate the total
+    public void calculate(){
+        System.out.println("Calculate func called");
+    }
     
-}
+    //Func to validate all fields are given
+    public void validate(){
+        
+        while(empty == true){
+             if (Income.getText().isEmpty()) {
+            actiontarget.setText("Please enter your income");
+            empty = true;
+            }
+            else if (Rent.getText().isEmpty()) {
+            actiontarget.setText("Please enter your Rent");
+            empty = true;
+            
+        }
+            else if (Util.getText().isEmpty()) {
+            actiontarget.setText("Please enter your Utilities");
+            empty = true;
+    }
+            else if (Groceries.getText().isEmpty()) {
+            actiontarget.setText("Please enter your Groceries");
+            empty = true;
+    }
+            else if (Travel.getText().isEmpty()) {
+            actiontarget.setText("Please enter your Travel costs");
+            empty = true;
+        }
+            else if (Debts.getText().isEmpty()) {
+            actiontarget.setText("Please enter your Debts");
+            empty = true;
+        }
+            else{
+                empty = false;
+                actiontarget.setText("");
+            }
+        }}}
+    
+ 
+
